69bd0691f465c7d8165868df7cc418ae9645e8d7
ZSabakh <zsabakhtarishvili@atu.edu>
2021-02-03 19:25:08 -0600

    Inpersistent drawing possible

--- app/src/main/java/com/example/drawapp/DrawView.kt
+++ app/src/main/java/com/example/drawapp/DrawView.kt
@@ -1,31 +1,27 @@
 package com.example.drawapp
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.*
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
-import java.nio.file.Path
-import java.util.*
-import java.util.jar.Attributes
 
-class DrawView(context: Context, attributes: AttributeSet) : View(context, attributes) {
+
+class DrawView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
     private var drawPath: CustomPath? = null
-    private var canvasBitmap: Bitmap? = null
+    private var brushSize: Float = 0.toFloat()
     private var drawPaint: Paint? = null
     private var canvasPaint: Paint? = null
-    private var brushSize: Float = 0.toFloat()
-    private var color = Color.RED
+    private var color = Color.BLACK
+    private var canvasBitmap: Bitmap? = null
     private var canvas: Canvas? = null
 
     init {
-        initDrawing()
+        setUpDrawing()
     }
 
-    private fun initDrawing(){
+    private fun setUpDrawing() {
         drawPath = CustomPath(color, brushSize)
         drawPaint = Paint()
         drawPaint!!.color = color
@@ -33,12 +29,61 @@ class DrawView(context: Context, attributes: AttributeSet) : View(context, attri
         drawPaint!!.strokeJoin = Paint.Join.ROUND
         drawPaint!!.strokeCap = Paint.Cap.ROUND
         canvasPaint = Paint(Paint.DITHER_FLAG)
+
         brushSize = 20.toFloat()
     }
 
-    internal inner class CustomPath(var color: Int,
-                                             var thickness: Float) : Path() {
-
+    override fun onSizeChanged(w: Int, h: Int, wprev: Int, hprev: Int) {
+        super.onSizeChanged(w, h, wprev, hprev)
+        canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+        canvas = Canvas(canvasBitmap!!)
     }
 
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        canvas.drawBitmap(canvasBitmap!!, 0f, 0f, canvasPaint)
+
+        if (!drawPath!!.isEmpty) {
+            drawPaint!!.strokeWidth = drawPath!!.brushThickness
+            drawPaint!!.color = drawPath!!.color
+            canvas.drawPath(drawPath!!, drawPaint!!)
+        }
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        val touchX = event.x // Touch event of X coordinate
+        val touchY = event.y // touch event of Y coordinate
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                drawPath!!.color = color
+                drawPath!!.brushThickness = brushSize
+
+                drawPath!!.reset()
+                drawPath!!.moveTo(
+                    touchX,
+                    touchY
+                )
+            }
+
+            MotionEvent.ACTION_MOVE -> {
+                drawPath!!.lineTo(
+                    touchX,
+                    touchY
+                )
+            }
+
+            MotionEvent.ACTION_UP -> {
+                drawPath = CustomPath(color, brushSize)
+            }
+            else -> return false
+        }
+
+        invalidate()
+        return true
+    }
+
+    internal inner class CustomPath(var color: Int, var brushThickness: Float) : Path()
 }
